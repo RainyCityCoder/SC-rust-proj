90966ff3013e0f203b85e6615332c93976c3ce3c
RainyCityCoder <cvmembership@posteo.net>
2023-11-13 17:52:36 -0800

    Move-as-one and for loop move working.

--- proj-2-smrt-ptr-move/main.cpp
+++ proj-2-smrt-ptr-move/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <memory>
 #include "print.h"
 
 struct holder {
@@ -11,33 +12,49 @@ struct holder {
 int main() {
   std::vector<int> vectorOne = {2,4,6};
   std::list<int> listOne = {1,3,5};
-  cout << "Start with vectorOne & listOne, we'll move them as one data structure: " << endl;
+  cout << "Moving vectorOne & listOne as one data structure, here are our starting list & vector respectively: " << endl;
   printList(listOne);
   printVector(vectorOne);
 
   holder holderA;
   holderA.vectorHolder.swap(vectorOne);
   holderA.listHolder.merge(listOne);
-  cout << "Put both into struct holderA, print original vector and list to show they're one data structure: " << endl;
-  printList(listOne);
-  printVector(vectorOne);
+  std::cout << "Put both into struct holderA, print both to show they're in one data structure: " << endl;
   printList(holderA.listHolder);
   printVector(holderA.vectorHolder);
+
+  std::unique_ptr<holder> pointer1 = make_unique<holder>(holderA);
 
   holder holderB {std::move(holderA)};
-  cout << "Move them to holderB, show original vector & list and holderA to show they're moved as one data structure: " << endl;
+  std::cout << "Move them to holderB, show original vector & list, now empty..." << endl;
   printList(listOne);
   printVector(vectorOne);
+  std::cout << "...and holderA, now empty..." << endl;
   printList(holderA.listHolder);
   printVector(holderA.vectorHolder);
+  std::cout << "...and holderB: " << endl;
   printList(holderB.listHolder);
   printVector(holderB.vectorHolder);
+  std::cout << "" << endl;
 
-  std::vector<int> vectorTwo = {2,4,6};
-  std::list<int> listTwo = {1,3,5};
-  cout << "Start with vectorTwo & listTwo, we'll move them element-by-element: " << endl;
-  printList(listTwo);
-  printVector(vectorTwo);
-
+  std::cout << "Moving elements individually: holderB's items back to holderA" << endl;
+  for (auto i : holderB.vectorHolder){
+    holderA.vectorHolder.push_back(i);
+    // holderB.vectorHolder.erase(i);
+  }
+  holderB.vectorHolder.clear();
+    
+  for (auto i : holderB.listHolder) {
+    holderA.listHolder.push_back(i);
+    // holderB.listHolder.erase(i);
+  }
+  holderB.listHolder.clear();
+  std::cout << "HolderB's list & vector respectively, now empty:" <<endl;
+  printList(holderB.listHolder);
+  printVector(holderB.vectorHolder);
+  std::cout << "HolderA's list & vector respectively:" <<endl;
+  printList(holderA.listHolder);
+  printVector(holderA.vectorHolder);
+  
   return 0;
 } 
